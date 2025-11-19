9d38288f87b6d8e18abeebd00b395e3baa0e2c9e
default <evan.maddicks@stu.ocsb.ca>
2025-11-19 00:48:04 +0000

    fixed

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+* @author Evan Maddicks
+* @version 1.0.0
+* @date 2025-11-12
+* @fileoverview this program calculates side lengths of a cube using its total volume
+*/
+let volume = 10 * 10 * 10
+let sides = volume / volume * 10
+
+
+console.log("The side lengths of a cube with a volume of " + volume + "mm³ = " + sides + "mm")
